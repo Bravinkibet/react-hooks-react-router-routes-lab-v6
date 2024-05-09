c6e29e2c6eaebc9db11b713465c1844051ce07c2
bravinkibetnyangusei <bravink599@gmail.com>
2024-05-09 19:06:24 +0300

    complete the lab

--- src/__tests__/Actors.test.js
+++ src/__tests__/Actors.test.js
@@ -1,79 +1,31 @@
-import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
-import { RouterProvider, createMemoryRouter} from "react-router-dom";
-import routes from "../routes";
+import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+import Card from "../components/ReusableCard";
 
-const actors = [
-  {
-    name: "Benedict Cumberbatch",
-    movies: ["Doctor Strange", "The Imitation Game", "Black Mass"],
-  },
-  {
-    name: "Justin Timberlake",
-    movies: ["Trolls", "Friends with Benefits", "The Social Network"],
-  },
-  {
-    name: "Anna Kendrick",
-    movies: ["Pitch Perfect", "Into The Wood"],
-  },
-  {
-    name: "Tom Cruise",
-    movies: [
-      "Jack Reacher: Never Go Back",
-      "Mission Impossible 4",
-      "War of the Worlds",
-    ],
-  },
-];
+function Actors() {
+  const [actors, setActors] = useState([])
 
-const router = createMemoryRouter(routes, {
-  initialEntries: [`/actors`],
-  initialIndex: 0
-})
-
-test("renders without any errors", () => {
-  const errorSpy = jest.spyOn(global.console, "error");
-
-  render(<RouterProvider router={router}/>);
-
-  expect(errorSpy).not.toHaveBeenCalled();
-
-  errorSpy.mockRestore();
-});
-
-test("renders 'Actors Page' inside of the <h1 />", () => {
-  render(<RouterProvider router={router}/>);
-  const h1 = screen.queryByText(/Actors Page/);
-  expect(h1).toBeInTheDocument();
-  expect(h1.tagName).toBe("H1");
-});
-
-test("renders each actor's name", async () => {
-  render(<RouterProvider router={router}/>);
-  for (const actor of actors) {
-    expect(
-      await screen.findByText(actor.name, { exact: false })
-    ).toBeInTheDocument();
-  }
-});
-
-test("renders a <li /> for each movie", async () => {
-  render(<RouterProvider router={router}/>);
-  for (const actor of actors) {
-    for (const movie of actor.movies) {
-      const li = await screen.findByText(movie, { exact: false });
-      expect(li).toBeInTheDocument();
-      expect(li.tagName).toBe("LI");
-    }
-  }
-});
-
-test("renders the <NavBar /> component", () => {
-  const router = createMemoryRouter(routes, {
-    initialEntries: ['/actors']
-  })
-  render(
-      <RouterProvider router={router}/>
+  useEffect(() =>{
+    fetch("http://localhost:4000/actors")
+      .then(r => r.json())
+      .then(data => setActors(data))
+      .catch(error => console.error(error));
+  }, []);
+  
+  const actorsList = actors.map(actor =>
+    <Card key={actor.id} name={actor.name} movies={actor.movies}/>
   );
-  expect(document.querySelector(".navbar")).toBeInTheDocument();
-});
+  return (
+    <>
+      <header>
+        <NavBar/>
+      </header>
+      <main>
+        <h1>Actors Page</h1>
+        {actorsList}
+      </main>
+    </>
+  );
+};
+
+export default Actors;
--- src/__tests__/Directors.test.js
+++ src/__tests__/Directors.test.js
@@ -1,71 +1,31 @@
-import "@testing-library/jest-dom";
-import { RouterProvider, createMemoryRouter} from "react-router-dom"
-import { render, screen } from "@testing-library/react";
-import routes from "../routes";
+import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+import Card from "../components/ReusableCard";
 
-const directors = [
-  {
-    name: "Scott Derrickson",
-    movies: ["Doctor Strange", "Sinister", "The Exorcism of Emily Rose"],
-  },
-  {
-    name: "Mike Mitchell",
-    movies: ["Trolls", "Alvin and the Chipmunks: Chipwrecked", "Sky High"],
-  },
-  {
-    name: "Edward Zwick",
-    movies: ["Jack Reacher: Never Go Back", "Blood Diamond", "The Siege"],
-  },
-];
+function Directors() {
+  const [directors, setDirectors] = useState([])
 
-const router = createMemoryRouter(routes, {
-  initialEntries: [`/directors`],
-  initialIndex: 0
-})
-
-test("renders without any errors", () => {
-  const errorSpy = jest.spyOn(global.console, "error");
-
-  render(<RouterProvider router={router}/>);
-
-  expect(errorSpy).not.toHaveBeenCalled();
-
-  errorSpy.mockRestore();
-});
-
-test("renders 'Directors Page' inside of a <h1 />", () => {
-  render(<RouterProvider router={router}/>);
-  const h1 = screen.queryByText(/Directors Page/);
-  expect(h1).toBeInTheDocument();
-  expect(h1.tagName).toBe("H1");
-});
-
-test("renders each director's name", async () => {
-  render(<RouterProvider router={router}/>);
-  for (const director of directors) {
-    expect(
-      await screen.findByText(director.name, { exact: false })
-    ).toBeInTheDocument();
-  }
-});
-
-test("renders a <li /> for each movie", async () => {
-  render(<RouterProvider router={router}/>);
-  for (const director of directors) {
-    for (const movie of director.movies) {
-      const li = await screen.findByText(movie, { exact: false });
-      expect(li).toBeInTheDocument();
-      expect(li.tagName).toBe("LI");
-    }
-  }
-});
-
-test("renders the <NavBar /> component", () => {
-  const router = createMemoryRouter(routes, {
-    initialEntries: ['/directors']
-  })
-  render(
-      <RouterProvider router={router}/>
+  useEffect(() =>{
+    fetch("http://localhost:4000/directors")
+      .then(r => r.json())
+      .then(data => setDirectors(data))
+      .catch(error => console.error(error));
+  }, []);
+  
+  const directorsList = directors.map(director =>
+    <Card key={director.id} name={director.name} movies={director.movies}/>
   );
-  expect(document.querySelector(".navbar")).toBeInTheDocument();
-});
+  return (
+    <>
+      <header>
+        <NavBar/>
+      </header>
+      <main>
+        <h1>Directors Page</h1>
+        {directorsList}
+      </main>
+    </>
+  );
+};
+
+export default Directors;
